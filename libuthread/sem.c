4d2b2e3f6afe2cb455d7c7bd48bf7dd4709ac6b1
hjkenned <hjkenned@ad3.ucdavis.edu@pc14.cs.ucdavis.edu>
2020-02-12 19:03:07 -0800

    Added basic naive implementation of tps; Needs debugging

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -10,21 +10,6 @@ struct semaphore {
 	queue_t blocked_threads;
 	// int lock;
 };
-
-// -- Static Functions -- //
-/*
-int test_and_set(int *mem){
-	int oldVal = *mem;
-	*mem = 1;
-	return oldVal;
-}
-static spinlock_aquire(int *lock) {
-	while (test_and_set(lock) == 1);
-}
-static spinlock_release(int *lock){
-	*lock = 0;
-}
-*/
 sem_t sem_create(size_t count)
 {
 	sem_t new_sem = malloc(sizeof(sem_t));
@@ -42,6 +27,7 @@ int sem_destroy(sem_t sem)
 	if(sem == NULL || queue_length(sem->blocked_threads)) {
 		return -1;
 	}
+	queue_destroy(sem->blocked_threads);
 	free(sem);
 	return 0;
 }
@@ -52,8 +38,10 @@ int sem_down(sem_t sem)
 		return -1;
 	}
 	enter_critical_section();
-	pthread_t *self = malloc(sizeof(pthread_t));
 	while(sem->count == 0){
+		// Need to malloc in while loop so that our corner case works correctly
+		// TODO: Make sure this isn't allocating excessive memory
+		pthread_t *self = malloc(sizeof(pthread_t));
 		*self = pthread_self();
 		queue_enqueue(sem->blocked_threads, (void*)self);
 		thread_block();
@@ -70,10 +58,8 @@ int sem_up(sem_t sem)
 	}
 	enter_critical_section();
 	sem->count += 1;
-	// TODO: Wake up next in queue
 	pthread_t *next_blocked_thread = NULL;
 	queue_dequeue(sem->blocked_threads, (void**)&next_blocked_thread);
-	// TODO: Make sure this works
 	if (next_blocked_thread != NULL) {
 		thread_unblock(*next_blocked_thread);
 		free(next_blocked_thread);
--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,35 +12,148 @@
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
+static queue_t pages;
 
-int tps_init(int segv)
+typedef struct mem_page_struct {
+	void* page_address;
+	pthread_t tid;
+} mem_page;
+
+static int find_tid(void *data, void *arg) {
+	mem_page *d = data;
+	pthread_t match = *(pthread_t*)arg;
+	if (d->tid == match) {
+		return 1;
+	}
+	return 0;
+}
+
+static int has_page(pthread_t tid, void **found) {
+	// Checks in pages if pages contains mem_page associated with TID
+	// If it does, place the pointer to said mem_page in found.
+	*found = NULL;
+	queue_iterate(pages, find_tid, &tid, (void**)found);
+	if (found == NULL) {
+		// We don't have a page
+		return 0;
+	}
+	return 1;
+}
+
+int tps_init(__attribute__((unused)) int segv)
 {
-	/* TODO: Phase 2 */
+	pages = queue_create();
+	return 0;
 }
 
 int tps_create(void)
 {
-	/* TODO: Phase 2 */
+	// TODO: Check if this is a critical section
+	enter_critical_section();
+	pthread_t tid = pthread_self();
+	//queue iterate on tid
+	mem_page* existing_page = NULL;
+	int cur_thread_has_page = has_page(tid, (void**)&existing_page);
+	if (cur_thread_has_page) {
+		// Has a page already
+		return -1;
+	}
+	mem_page* new_mem_page = malloc(sizeof(mem_page));
+	void* addr = mmap(NULL, TPS_SIZE, PROT_READ|PROT_WRITE,
+		MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	new_mem_page->tid = tid;
+	new_mem_page->page_address = addr;
+	queue_enqueue(pages, new_mem_page);
+	exit_critical_section();
+	return 0;
 }
 
 int tps_destroy(void)
 {
-	/* TODO: Phase 2 */
+	enter_critical_section();
+	pthread_t tid = pthread_self();
+	//queue iterate on tid
+	mem_page* existing_page = NULL;
+	int cur_thread_has_page = has_page(tid, (void**)&existing_page);
+	if (!cur_thread_has_page) return -1;
+	queue_delete(pages, (void*)existing_page);
+	munmap(existing_page->page_address, TPS_SIZE);
+	free(existing_page);
+	exit_critical_section();
+	return 0;
 }
 
 int tps_read(size_t offset, size_t length, void *buffer)
 {
-	/* TODO: Phase 2 */
+	if (buffer == NULL) return -1;
+	if ((length + offset) > TPS_SIZE){
+		return -1;
+	}
+	enter_critical_section();
+	pthread_t tid = pthread_self();
+	mem_page* existing_page = NULL;
+	int cur_thread_has_page = has_page(tid, (void**)&existing_page);
+	if (!cur_thread_has_page) return -1;
+
+	void* tps_address = existing_page->page_address;
+	size_t count = offset;
+	int buf_count = 0;
+	for(;count<length;count++){
+		((char*)buffer)[buf_count++] = ((char*)tps_address)[count];
+	}
+	exit_critical_section();
+	return 0;
 }
 
 int tps_write(size_t offset, size_t length, void *buffer)
 {
-	/* TODO: Phase 2 */
+	// TODO: Check if shared TPS
+	if (buffer == NULL) return -1;
+	if ((length + offset) > TPS_SIZE){
+		return -1;
+	}
+	enter_critical_section();
+	pthread_t tid = pthread_self();
+	mem_page* existing_page = NULL;
+	int cur_thread_has_page = has_page(tid, (void**)&existing_page);
+	if (!cur_thread_has_page) return -1;
+	void* tps_address = existing_page->page_address;
+	size_t count = offset;
+	int buf_count = 0;
+	for(;count<length;count++){
+		((char*)tps_address)[count] = ((char*)buffer)[buf_count++];
+	}
+	exit_critical_section();
+	return 0;
 }
 
 int tps_clone(pthread_t tid)
 {
-	/* TODO: Phase 2 */
+	enter_critical_section();
+	pthread_t cur_tid = pthread_self();
+	mem_page* existing_page = NULL;
+	// Check if the current thread has a page or not
+	queue_iterate(pages, find_tid, (void*)cur_tid, (void**)existing_page);
+	if (existing_page != NULL) {
+		// Cur_thread already has a TPS
+		return -1;
+	}
+	mem_page *src_page = NULL;
+	// Check if the new tid doesn't have a TPS
+	queue_iterate(pages, find_tid, (void*)tid, (void**)src_page);
+	if (src_page == NULL) {
+		// @tid doesn't have a tps
+		return -1;
+	}
+	// If we get here, we know cur_thread does NOT have a TPS and @TID
+	// DOES have a tps
+	// Create a new TPS associated with the current tid
+	tps_create();
+	// Get the ptr to the new page
+	mem_page *dst_page = NULL;
+	queue_iterate(pages, find_tid, (void*)cur_tid, (void**)dst_page);
+	memcpy(dst_page->page_address,
+		src_page->page_address, TPS_SIZE);
+	exit_critical_section();
+	return 0;
 }
-

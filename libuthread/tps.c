6dda422b6ef1d3731843b77f88536fafb8391531
hjkenned <hjkenned@ad3.ucdavis.edu@pc14.cs.ucdavis.edu>
2020-02-14 19:06:05 -0800

    Finished tps, added protection and copy-on-write

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,19 +12,18 @@
 #include "thread.h"
 #include "tps.h"
 
-static queue_t pages;
-typedef struct page_struct{
+static queue_t tps_queue;
+struct page_struct{
 	void* page_address;
 	size_t reference_counter;
 };
-typedef struct mem_page_struct {
-	struct page_struct *page_address;
+typedef struct tps_struct {
+	struct page_struct *mem_page;
 	pthread_t tid;
-} mem_page;
-
-// Find the mempage associated with tid @arg
+} tps;
+// Find the tps associated with tid @arg
 static int find_tid(void *data, void *arg) {
-	mem_page *d = data;
+	tps *d = data;
 	pthread_t match = *(pthread_t*)arg;
 	if (d->tid == match) {
 		return 1;
@@ -33,25 +32,13 @@ static int find_tid(void *data, void *arg) {
 }
 
 static int find_tps_area(void* data, void* arg){
-	mem_page *d = data;
-	if (d->page_address == arg) {
+	tps *d = data;
+	if (d->mem_page->page_address == arg) {
 		return 1;
 	}
 	return 0;
 }
-/*
-static int has_page(pthread_t tid, void **found) {
-	// Checks in pages if pages contains mem_page associated with TID
-	// If it does, place the pointer to said mem_page in found.
-	*found = NULL;
-	queue_iterate(pages, find_tid, &tid, (void**)found);
-	if (found == NULL) {
-		// We don't have a page
-		return 0;
-	}
-	return 1;
-}
-*/
+
 static void segv_handler(int sig, siginfo_t *si,
 	__attribute__((unused)) void *context)
 {
@@ -64,8 +51,9 @@ static void segv_handler(int sig, siginfo_t *si,
 	/*
 	 * Iterate through all the TPS areas and find if p_fault matches one of them
 	 */
-	mem_page *guilty_page = NULL;
-	queue_iterate(pages, find_tps_area, p_fault, (void**)&guilty_page);
+	 //??
+	struct page_struct *guilty_page = NULL;
+	queue_iterate(tps_queue, find_tps_area, p_fault, (void**)&guilty_page);
 	if (guilty_page)
 		/* Printf the following error message */
 		fprintf(stderr, "TPS protection error!\n");
@@ -85,8 +73,8 @@ int tps_init(int segv)
 		sigaction(SIGBUS, &sa, NULL);
 		sigaction(SIGSEGV, &sa, NULL);
 	}
-	//create static queue to hold mem pages
-	pages = queue_create();
+	//create static queue to hold mem tps_queue
+	tps_queue = queue_create();
 	return 0;
 }
 
@@ -96,17 +84,23 @@ int tps_create(void)
 	enter_critical_section();
 	pthread_t tid = pthread_self();
 	// Find out whether the current has a tps already
-	mem_page* existing_page = NULL;
-	queue_iterate(pages, find_tid, (void*)tid, (void**)existing_page);
+	tps* existing_tps = NULL;
+	queue_iterate(tps_queue, find_tid, (void*)tid, (void**)existing_tps);
 	// If we GET a page back, we already have a tps
-	if (existing_page != NULL) return -1;
-	// Otherwise we create a new memory page to hold the tps
-	mem_page* new_mem_page = malloc(sizeof(mem_page));
+	if (existing_tps != NULL) return -1;
+	// Otherwise we allocate a tps
+	tps* new_tps = malloc(sizeof(tps));
+	// Allocate the page struct to hold the address and reference_counter
+	struct page_struct* new_page = malloc(sizeof(struct page_struct));
+	// Set the new tps' page to new_page
+	new_tps->mem_page = new_page;
+	// Initialize the reference_counter to zero
+	new_tps->mem_page->reference_counter = 1;
 	void* addr = mmap(NULL, TPS_SIZE, PROT_NONE,
 		MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	new_mem_page->tid = tid;
-	new_mem_page->page_address = addr;
-	queue_enqueue(pages, new_mem_page);
+	new_tps->tid = tid;
+	new_tps->mem_page->page_address = addr;
+	queue_enqueue(tps_queue, new_tps);
 	exit_critical_section();
 	return 0;
 }
@@ -116,13 +110,14 @@ int tps_destroy(void)
 	enter_critical_section();
 	pthread_t tid = pthread_self();
 	//queue iterate on tid
-	mem_page* existing_page = NULL;
-	queue_iterate(pages, find_tid, (void*)tid, (void**)&existing_page);
+	tps* existing_tps = NULL;
+	queue_iterate(tps_queue, find_tid, (void*)tid, (void**)&existing_tps);
 	// Make sure that the page actually exists:
-	if (existing_page == NULL) return -1;
-	queue_delete(pages, (void*)existing_page);
-	munmap(existing_page->page_address, TPS_SIZE);
-	free(existing_page);
+	if (existing_tps == NULL) return -1;
+	queue_delete(tps_queue, (void*)existing_tps);
+	munmap(existing_tps->mem_page->page_address, TPS_SIZE);
+	free(existing_tps->mem_page);
+	free(existing_tps);
 	exit_critical_section();
 	return 0;
 }
@@ -137,11 +132,11 @@ int tps_read(size_t offset, size_t length, void *buffer)
 	}
 	enter_critical_section();
 	pthread_t tid = pthread_self();
-	mem_page* existing_page = NULL;
-	queue_iterate(pages, find_tid, (void*)tid, (void**)&existing_page);
+	tps* existing_tps = NULL;
+	queue_iterate(tps_queue, find_tid, (void*)tid, (void**)&existing_tps);
 	// Make sure the page actually exists:
-	if (existing_page == NULL) return -1;
-	void* tps_address = existing_page->page_address;
+	if (existing_tps == NULL) return -1;
+	void* tps_address = existing_tps->mem_page->page_address;
 	mprotect(tps_address, TPS_SIZE, PROT_READ);
 	memcpy(buffer, tps_address + offset, length);
 	mprotect(tps_address, TPS_SIZE, PROT_NONE);
@@ -152,18 +147,36 @@ int tps_read(size_t offset, size_t length, void *buffer)
 int tps_write(size_t offset, size_t length, void *buffer)
 {
 	// TODO: Check if shared TPS
+	// Error checking:
 	if (buffer == NULL) return -1;
 	if ((length + offset) > TPS_SIZE){
 		return -1;
 	}
 	enter_critical_section();
 	pthread_t tid = pthread_self();
-	mem_page* existing_page = NULL;
-	queue_iterate(pages, find_tid, (void*)tid, (void**)&existing_page);
-	// make sure the page actually exists:
-	if (existing_page == NULL) return -1;
-	// Cur_thread does not have a TPS
-	void* tps_address = existing_page->page_address;
+	tps* existing_tps = NULL;
+	queue_iterate(tps_queue, find_tid, (void*)tid, (void**)&existing_tps);
+	// Make sure the page actually exists:
+	if (existing_tps == NULL) return -1;
+	// Do I need to perform a clone on the page?
+	if (existing_tps->mem_page->reference_counter > 1){
+		void *old_page_address = existing_tps->mem_page->page_address;
+		// Copy:
+		void *addr = mmap(NULL, TPS_SIZE, PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		// Add read access to exisitng address
+		mprotect(old_page_address, TPS_SIZE, PROT_READ);
+		memcpy(addr, old_page_address, TPS_SIZE);
+		mprotect(addr, TPS_SIZE, PROT_NONE);
+		mprotect(old_page_address, TPS_SIZE, PROT_NONE);
+		existing_tps->mem_page->reference_counter--;
+		struct page_struct *new_page = malloc(sizeof(struct page_struct));
+		new_page->reference_counter = 1;
+		new_page->page_address = addr;
+		existing_tps->mem_page = new_page;
+	}
+	// Write:
+	void* tps_address = existing_tps->mem_page->page_address;
 	mprotect(tps_address, TPS_SIZE, PROT_WRITE);
 	memcpy(tps_address + offset, buffer, length);
 	mprotect(tps_address, TPS_SIZE, PROT_NONE);
@@ -175,25 +188,30 @@ int tps_clone(pthread_t tid)
 {
 	enter_critical_section();
 	pthread_t cur_tid = pthread_self();
-	mem_page* existing_page = NULL;
+	tps *existing_tps = NULL;
 	// Check if the current thread has a page or not
-	queue_iterate(pages, find_tid, (void*)cur_tid, (void**)&existing_page);
+	queue_iterate(tps_queue, find_tid, (void*)cur_tid, (void**)&existing_tps);
 	// if we get something back, cur_thread already has a TPS!
-	if (existing_page != NULL) return -1;
+	if (existing_tps != NULL) return -1;
 	// Check if the new tid doesn't have a TPS
-	mem_page *src_page = NULL;
-	queue_iterate(pages, find_tid, (void*)tid, (void**)&src_page);
+	tps *src_page = NULL;
+	queue_iterate(tps_queue, find_tid, (void*)tid, (void**)&src_page);
 	// if we don't get anything back the page does not exist
 	if (src_page == NULL) return -1;
 	// If we get here, we know cur_thread does NOT have a TPS and @TID
 	// DOES have a tps
-	// Create a new TPS associated with the current tid
-	tps_create();
-	// Get the ptr to the page we just created
-	mem_page *dst_page = NULL;
-	queue_iterate(pages, find_tid, (void*)cur_tid, (void**)&dst_page);
-	// TODO: Implement with tps_read, tps_write
-	memcpy(dst_page->page_address, src_page->page_address, TPS_SIZE);
+
+	// Create a TPS for myself
+	tps *new_tps = malloc(sizeof(tps));
+	// Set my mem page to target's mem_page
+	new_tps->mem_page = src_page->mem_page;
+	// Increment the reference counter
+	new_tps->mem_page->reference_counter++;
+
+	// Associate myself with the new_tps
+	new_tps->tid = cur_tid;
+	queue_enqueue(tps_queue, new_tps);
+
 	exit_critical_section();
 	return 0;
 }
